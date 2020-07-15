f69dccd1638ea979ec85d0c02241acf7ed48668f
rob5300 <rob5300@hotmail.com>
2020-07-16 00:08:40 +0100

    Working mask creation when detected in the vmt but slow!

--- __init__.py
+++ __init__.py
@@ -10,39 +10,85 @@ bl_info = {
 
 import bpy
 import os
+import re
+from bpy_extras.io_utils import ImportHelper
+from bpy.props import StringProperty, BoolProperty, EnumProperty
+from bpy.types import Operator
 
-imagesPath = "D:\\TF2 Stuff\\ModelPreview\\tf2 assets\\All Source VTF\\PNG"
+imagesPath = r"D:\TF2 Stuff\ModelPreview\tf2 assets\All Source VTF\PNG"
+images = None
+vmtsPath = r"D:\TF2 Stuff\ModelPreview\tf2 assets\All Source VMT"
+vmts = None
+outputPath = r"D:\TF2 Stuff\ModelPreview\tf2 assets\Output"
+
+logfilename = r"vmtcoloursoutput"
+
+param1 = "___$color2"
+param2 = "$colortint_base"
+
+num_regex = "([^_, ,\D][0-9]{1,3})"
             
-def ReadFile(context, filepath):
-
+def ReadQC(context, filepath):
     bpy.ops.import_scene.smd(filepath = filepath)
 
     bpy.ops.object.select_all(action="DESELECT")
 
     for ob in bpy.context.scene.objects:
-         if("lod" in ob.name and "lod_1" not in ob.name):
+         if(("lod" in ob.name and "lod_1" not in ob.name) or ("VTA" in ob.name)):
             ob.select_set(True)
             bpy.ops.object.delete()
 
     for ob in bpy.context.scene.objects:
         for mat_slot in ob.material_slots:
             if(mat_slot.material != None):
-                SetupMaterial(mat_slot.material, FindImageWithName(mat_slot.material.name, "color"))
+                matImage = FindImageWithName(mat_slot.material.name, "color")
 
-    bpy.ops.export_scene.gltf()
+                #Texture not found, we need to search the vmt for its real name
+                if(matImage == None):
+                    matImage = GetMainTextureNameFromVMT(mat_slot.material.name)
+
+                SetupMaterial(mat_slot.material, FindImageWithName(matImage, "color"))
+
+    #bpy.ops.export_scene.gltf()
     
     return {'FINISHED'}
 
 def FindImageWithName(name, suffix):
-    files = os.listdir(imagesPath)
+    global images #Stupid
+    if(images == None):
+        images = os.listdir(imagesPath)
 
-    for file in files:
+    for file in images:
         fname = os.path.splitext(os.path.basename(file))[0]
         if(fname == name or fname == "{name}_{suffix}"):
             return os.path.join(imagesPath, file)
 
     return None
 
+def FindVMTWithName(name):
+    global vmts #Stupid
+    if(vmts == None):
+        vmts = os.listdir(vmtsPath)
+    
+    if("{name}.vmt" in vmts):
+        return os.path.join(vmtsPath, "{name}.vmt")
+    elif("{name}_color.vmt" in vmts):
+        return os.path.join(vmtsPath, "{name}_color.vmt")
+
+    return ""
+
+def GetMainTextureNameFromVMT(vmtName):
+    vmt = open(os.path.join(vmtsPath, vmtName + ".vmt"), mode="r")
+    vmt_text = vmt.readlines()
+
+    for line in vmt_text:
+        if("$basetexture" in line):
+            splitLine = line.split('"')
+            for i in range(0, len(splitLine)):
+                if("/" in splitLine[i]):
+                    pathSplit = splitLine[i].split("/")
+                    return pathSplit[len(pathSplit) - 1]
+        
 def SetupMaterial(material, mainTex):
     material.use_nodes = True
 
@@ -58,25 +104,30 @@ def SetupMaterial(material, mainTex):
     tex_node = nodes.new(type="ShaderNodeTexImage")
     if(mainTex != None):
         tex_node.image = getCyclesImage(mainTex)
-        CreateMaskTexture(tex_node.image)
         links.new(tex_node.outputs["Color"], principled.inputs[0])
 
-def HasAlpha(image):
+    #Find vmt for this material. If there is a color in this, output it and also try to produce a mask image for this materials main texture.
+    color = GetHexFromVMT(os.path.join(vmtsPath, material.name + ".vmt"))
+    if(color != ""):
+        #We have a material that should require a mask. Lets make one
+        CreateMaskTexture(getCyclesImage(mainTex), os.path.join(outputPath, "{material.name}_mask"))
 
-    return None
-
-def CreateMaskTexture(image):
+def CreateMaskTexture(image, destination):
     mask = image.copy()
     mask.name = image.name + "_ColourMask"
-    mask.pixels[0] = 255
+    channels = image.channels
+    size = image.size[0]*image.size[1]
 
-    return None
+    i = channels - 1
+    while(i < size):
+        alpha = image.pixels[i]
 
-# ImportHelper is a helper class, defines filename and
-# invoke() function which calls the file selector.
-from bpy_extras.io_utils import ImportHelper
-from bpy.props import StringProperty, BoolProperty, EnumProperty
-from bpy.types import Operator
+        for x in range(i - channels - 1, channels - 1):
+            mask.pixels[x] = alpha
+
+        i = i + channels
+
+    mask.save_render(destination)
 
 class ImportSomeData(Operator, ImportHelper):
     """Import fbx props using a .vmf"""
@@ -93,18 +144,15 @@ class ImportSomeData(Operator, ImportHelper):
     )
 
     def execute(self, context):
-        return ReadFile(context, self.filepath)
-
+        return ReadQC(context, self.filepath)
 
 # Only needed if you want to add into a dynamic menu
 def menu_func_import(self, context):
     self.layout.operator(ImportSomeData.bl_idname, text="Convert QC")
 
-
 def register():
     bpy.utils.register_class(ImportSomeData)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
-
 
 def unregister():
     bpy.utils.unregister_class(ImportSomeData)
@@ -117,6 +165,22 @@ def getCyclesImage(imgpath):
             return img
     return bpy.data.images.load(imgpath)
 
+def GetHexFromVMT(vmt_path):
+    filehandle = open(vmt_path, mode='r')
+    file_text = filehandle.readlines()
+
+    for line in file_text:
+        if param1 in line or param2 in line:
+            if "255 255 255" not in line and "{" in line and "}" in line and "resultVar" not in line and "srcVar" not in line:
+                hex = GetHexColour(line)
+
+def GetHexColour(line):
+    toreturn = ""
+    result = re.findall(num_regex, line)
+    for r in result:
+        toreturn += hex(int(r)).replace("0x", "")
+
+    return toreturn
 
 if __name__ == "__main__":
     register()
